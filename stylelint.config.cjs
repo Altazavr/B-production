7482c4861350e81a10abc5cf18a207b8a8195c4b
Altazavr <tzenpauver@gmail.com>
2025-10-30 14:00:20 +0500

    testing ci

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,8 +1,11 @@
 // stylelint.config.cjs
 module.exports = {
     extends: ['stylelint-config-standard-scss'],
+    customSyntax: 'postcss-scss',
     rules: {
         'selector-class-pattern': null,
         'number-max-precision': [4, { severity: 'warning' }],
+        indentation: null,
+        'no-missing-end-of-source-newline': null,
     },
 };
