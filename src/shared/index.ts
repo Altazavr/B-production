11b9afab905658fe4b3eb93c0f2ff3eaf70ed911
Altazavr <tzenpauver@gmail.com>
2025-11-10 21:37:42 +0500

    fix tests

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,4 +3,4 @@ export { AppLink } from './ui/AppLink/AppLink';
 export { PageLoader } from './ui/PageLoader/PageLoader';
 export { Button, ThemeButton, ButtonSize } from './ui/Button/Button';
 export { ErrorPage } from './ui/ErrorPage/ErrorPage';
-export { renderWithTranslation } from './lib/renderWithTranslation/renderWithTranslation';
+export { componentRender } from './lib/tests/componentRender/componentRender';
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared';
+import { componentRender } from 'shared';
 import { Sidebar } from 'widgets/Sidebar';
 
 describe('Button', () => {
     test('Test render', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('Test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('toggleBtn');
         fireEvent.click(toggleBtn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
