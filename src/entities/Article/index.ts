817087da4c2c83ca82f319f6df6196a230e9f793
Altazavr <tzenpauver@gmail.com>
2026-07-06 21:58:26 +0500

    add article list, article item, skeleton and stories

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,6 +1,6 @@
 export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
 export type { Article } from './model/types/article';
-export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
+export type { ArticleDetailsSchema } from './model/types/ArticleDetailsSchema';
 export {
     getArticleDetailsData,
     getArticleDetailsIsLoading,
